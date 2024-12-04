983114af2d9ffe8b165d0a20f92dc2ef942539a7
Preu Sharma <preu1061.be21@chitkara.edu.in>
2024-12-04 15:17:34 +0530

    [Preu] Add:adding Employee.js

---
+++ Employee.js
@@ -0,0 +1,19 @@
+const Present=1;
+const Absent=0;
+let isPresent=false;    
+function checkAttendence()
+{
+    let randomValue=Math.floor(Math.random()*2);
+    console.log("Random Value Selected: "+randomValue)
+    if(randomValue==Present)
+    {
+        console.log("Employee is present");
+        isPresent=true;                        
+    }
+    else{
+        console.log("employee is absent");
+    }
+    console.log(isPresent)         
+    return isPresent;          
+}
+checkAttendence();
